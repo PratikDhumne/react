0f69fba4c853c85e757984064c0739fc997acc7b
PratikDhumne <pratikdhumne2000@gmail.com>
2025-04-08 15:23:22 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+
+//PRIMITIVE DATATYPES
+// number => 2 to power 53
+//bignit
+//string => ""
+//boolean => true/false
+//null =>stand alone value
+//undefined 
+//symbol => unique
+
+
+//object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
